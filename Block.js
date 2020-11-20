af6be9eeb1d7d700d1ce10704079e4f200903795
ShreevarGupta <71551218+ShreevarGupta@users.noreply.github.com>
2020-11-20 23:56:35 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,46 @@
+class Block {
+
+    constructor(x, y, width, height) {
+
+        var options={
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visibility = 255;
+        World.add(world, this.body);
+
+      }
+
+      display() {
+
+        if(this.body.speed < 3) {
+          var angle = this.body.angle;
+          push();
+          translate(this.body.position.x, this.body.position.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0, 0, this.width, this.height);
+          pop();
+        }
+
+        else{
+          World.remove(world, this.body);
+          push();
+          this.Visiblity = this.Visiblity - 5;
+          pop();
+        }
+
+      }
+
+      score() {
+        if(this.Visibility < 0){
+          score = score + 1;
+        }
+      }
+      
+}
